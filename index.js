cbd0d3dfc7ce6b2a9d4762d8922ee6106ddf9731
Aswathy-Sivadas <aswathydas612@gmail.com>
2025-05-18 15:04:56 +0530

    pass is AchuDasTheMass

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import bodyParser from "body-parser";
+
+var app= express();
+var port=3000;
+var __dirname=dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/check",(req,res)=>
+{
+    var pass=req.body["password"];
+    if(pass=="AchuDasTheMass")
+    {
+        res.sendFile(__dirname+"/public/secret.html");
+    }
+    else{
+        res.sendFile(__dirname+"/public/index.html");
+    }
+})
+app.get("/",(req,res)=>
+{
+    res.sendFile(__dirname+"/public/index.html");
+})
+
+app.listen(port,(req,res)=>{
+    console.log("listening to port "+port);
+})
